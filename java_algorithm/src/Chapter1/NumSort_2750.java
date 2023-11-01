d90021271f88d916e6e2fbfc73065c7b8d7855a9
syoo <syoo@bizflow.com>
2023-11-02 04:03:24 +0900

    [Algorithm] chapter1 - 2750

---
+++ java_algorithm/src/Chapter1/NumSort_2750.java
@@ -0,0 +1,25 @@
+package Chapter1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class NumSort_2750 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		int A[] = new int[num];
+		
+		
+		for(int i = 0; i < num; i++) {
+			A[i] = sc.nextInt();
+		}
+		Arrays.sort(A);
+		
+		for(int n : A) {
+			System.out.println(n);
+		}
+	}
+
+}
