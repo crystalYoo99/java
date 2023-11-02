a3e7cd121d0a9de9982570b80884eaecf79bc6b1
syoo <syoo@bizflow.com>
2023-11-03 02:10:28 +0900

    [Algorithm] chapter3 - sum, avg, prefix sum

---
+++ java_algorithm/src/Chapter3/NumSum_11720.java
@@ -0,0 +1,21 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class NumSum_11720 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		String s = sc.next();
+		char c[] = s.toCharArray();
+		int sum = 0;
+		
+		for (int i = 0; i < num; i++) {
+			sum += c[i] - '0';
+		}
+		
+		System.out.println(sum);
+	}
+
+}
