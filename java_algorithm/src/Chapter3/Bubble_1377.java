d5f8ff8609695dc77ea7d6ad11aee4397f6be0fd
syoo <syoo@bizflow.com>
2023-11-19 23:44:59 +0900

    [Algorithm] chapter3 - Stack, Heap

---
+++ java_algorithm/src/Chapter3/Bubble_1377.java
@@ -0,0 +1,43 @@
+package Chapter3;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.io.IOException;
+
+public class Bubble_1377 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        mData str[] = new mData[N];
+        
+        for (int i =0; i < N; i++) {
+            str[i] = new mData(Integer.parseInt(br.readLine()), i);
+        }
+        
+        Arrays.sort(str);
+        int max = 0;
+        for (int i =0; i < N; i++) {
+            if (max < str[i].index - i) {
+                max = str[i].index-i;
+            }
+        }
+        System.out.println(max + 1);
+    }
+}
+class mData implements Comparable<mData> {
+	int value;
+	int index;
+	
+	public mData(int value, int index) {
+		super();
+		this.value = value;
+		this.index = index;
+	}
+        
+    @Override
+    public int compareTo(mData o) {
+        return this.value - o.value;
+    }
+}
---
+++ java_algorithm/src/Chapter3/Sort_2750.java
@@ -0,0 +1,27 @@
+package Chapter3;
+
+import java.util.Scanner;
+
+public class Sort_2750 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int arr[] = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int i =0; i < N; i++) {
+            for (int j = 0; j < N-i-1; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (int i =0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
