a926751699bba736d5c320603c6e4c5066c2462a
syoo <syoo@bizflow.com>
2023-11-25 00:17:13 +0900

    [algorithm] Chapter4 - Bubble2 fix

--- java_algorithm/src/Chapter4/Bubble2_1517.java
+++ java_algorithm/src/Chapter4/Bubble2_1517.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.util.StringTokenizer;
 
-public class Bubble2_1571 {
+public class Bubble2_1517 {
 	public static int N;
 	public static int A[], tmp[];
 	public static long answer;
@@ -21,12 +21,48 @@ public class Bubble2_1571 {
 		for(int i = 1; i <= N; i++) {
 			A[i] = Integer.valueOf(st.nextToken());
 		}
+		answer = 0;
 		merge(1, N);
 		
 		System.out.println(answer);
 	}
 	
-	public static void merge(int s, int e) {
+	private static void merge(int s, int e) {
+		if (e - s < 1) {
+			return;
+		}
+		int m = s + (e - s)/2;
 		
+		merge(s, m);
+		merge(m+1, e);
+		
+		for (int i = s; i <= e; i++) {
+			tmp[i] = A[i];
+		}
+		int k = s;
+		int idx1 = s;
+		int idx2 = m+1;
+		while (idx1 <= m && idx2 <= e) {
+			if (tmp[idx1] > tmp[idx2]) {
+				A[k] = tmp[idx2];
+				answer = answer + idx2 - k;
+				k++;
+				idx2++;
+			} else {
+				A[k] = tmp[idx1];
+				k++;
+				idx1++;
+			}
+		}
+		while (idx1 <= m) {
+			A[k] = tmp[idx1];
+			k++;
+			idx1++;
+		}
+		while (idx2 <= e) {
+			A[k] = tmp[idx2];
+			k++;
+			idx2++;
+		}
 	}
 }
