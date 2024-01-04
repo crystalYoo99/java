9947d9432e9648ee55d95d165ab3d67bf36d887f
syoo <syoo@bizflow.com>
2024-01-04 23:21:27 +0900

    [modern] 1/4

---
+++ modern-java-in-action/chap11/optionalExample/CarOptional.java
@@ -0,0 +1,10 @@
+package chap11.optionalExample;
+
+import java.util.Optional;
+
+public class CarOptional {
+    private Optional<Insurance> insurance;
+    public Optional<Insurance> getInsurance() {
+        return insurance;
+    }
+}
---
+++ modern-java-in-action/chap11/optionalExample/PersonOptional.java
@@ -0,0 +1,9 @@
+package chap11.optionalExample;
+import java.util.Optional;
+
+public class PersonOptional {
+    private Optional<Car> car;
+    public Optional<Car> getCar() {
+        return car;
+    }
+}
