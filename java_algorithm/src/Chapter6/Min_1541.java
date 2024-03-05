21d5e53bbc683445925f63046101a3f1af3543d4
crystalYoo99 <yoosujeong99@gmail.com>
2024-03-05 23:18:14 +0900

    [study] 0305

---
+++ java_algorithm/src/Chapter6/Min_1541.java
@@ -0,0 +1,24 @@
+package Chapter6;
+
+import java.util.*;
+import java.io.*;
+
+public class Min_1541 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String[] A = s.split("-");
+        int Sum = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            int temp_sum = 0;
+            String[] temp = A[i].split("\\+");
+            for (int j = 0; j < temp.length; j++) {
+                temp_sum += Integer.parseInt(temp[j]);
+            }
+            if (i == 0) Sum += temp_sum;
+            else Sum -= temp_sum;
+        }
+        System.out.println(Sum);
+    }
+}
