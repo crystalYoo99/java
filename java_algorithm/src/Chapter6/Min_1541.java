9d0acb98171a42b1d7e0c7e73078d5d9927b3837
syoo <syoo@bizflow.com>
2024-03-05 18:50:48 +0900

    [study] 0305

---
+++ java_algorithm/src/Chapter6/Min_1541.java
@@ -0,0 +1,19 @@
+package Chapter6;
+
+import java.util.*;
+import java.io.*;
+public class Min_1541 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        String[] inputArray = input.split("-");
+        int[] minusSum = new int[inputArray.length];
+
+        for (int i = 0; i < inputArray.length; i++) {
+
+        }
+
+        int Sum =
+
+    }
+}
